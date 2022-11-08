4e666d9844f95f06af5bd917aa62d5caa7359d19
Mateus Maranhão <feradourada@gmail.com>
2022-11-08 12:56:58 -0300

    implement Footer

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="home">
-        <div class="row">
+        <div class="container">
+            <div class="row">
             <div class="col-md-6">
                 <img src="../../../public/img/home.jpg" class="img-fluid" />
             </div>
@@ -8,11 +9,26 @@
                 <h3>Espaço Lírio, saúde e bem estar</h3>
             </div>
         </div>
+        </div>
     </section>
     <section class="texto-home">
         <h2>Acupuntura, cone chinês, auriculoterapia, ventosas, quick massagem, curso de autoconhecimento</h2>
     </section>
+    <Footer></Footer>
 </template>
+
+<script>
+
+    import Footer from '@/components/Footer/Footer.vue'
+
+    export default {
+        name: 'Home',
+        components: {
+            Footer
+        }
+    }
+
+</script>
 
 <style lang="scss">
     @import 'Home'; 
